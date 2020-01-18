c76758778fad37e547599ef27a4938af4a863b34
Murilo Araujo <muernandes@icloud.com>
2020-01-18 20:15:34 -0300

    added libraries

---
+++ src/pages/home/home.component.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import "./home.styles.scss";
+export default class Home extends Component {
+  render() {
+    return <div> textInComponent </div>;
+  }
+}
